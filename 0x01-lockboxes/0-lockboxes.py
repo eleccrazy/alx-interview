375560ed97e1374008fa23c3e143a34189ad7937
Gizachew Bayness <eleccrazy24@gmail.com>
2023-05-01 22:14:38 +0300

    Adding comments

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -9,17 +9,23 @@ Date Created: Jan 31, 2023
 
 
 def canUnlockAll(boxes):
-    """A function that checks if all the boxes can be opened"""
-    keys = []
+    """A function that checks if all boxes can be opened"""
+    keys = []  # List to store unique keys to unlock boxes
     length = len(boxes)
     idx = 0
 
+    # Iterate over the boxes
     for i in boxes:
+        # Iterate over numbers in each boxes
         for j in i:
+            # Store unique numbers from boxes as keys
             if j not in keys and j != 0 and j != idx:
                 keys.append(j)
         idx += 1
+    # Sort the keys in ascending order
     keys = sorted(keys)
+    # Iterate over the range of the length of the key, and make sure that each
+    # box is opened by the keys.
     for k in range(1, length):
         if k != keys[k - 1]:
             return False
