c2a352575bd756b5926fbffc248331b177e2a67d
Gizachew Bayness <eleccrazy24@gmail.com>
2023-05-12 19:29:35 +0300

    Completed

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,44 +1,48 @@
 #!/usr/bin/python3
-
-"""script that reads stdin line by line and computes metrics """
-
-import sys
-
-
-def printStatus(dic, size):
-    """ Prints information """
-    print("File size: {:d}".format(size))
-    for i in sorted(dic.keys()):
-        if dic[i] != 0:
-            print("{}: {:d}".format(i, dic[i]))
+"""
+File: 0-status.py
+Description: A module that contains code for solving log parsing problem
+             in python.
+Date Created: May 12, 2023
+Author: Gizachew Bayness
+"""
+from sys import stdin
 
 
-statusCodes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
-               "404": 0, "405": 0, "500": 0}
-count = 0
-size = 0
-
-try:
-    for line in sys.stdin:
-        if count != 0 and count % 10 == 0:
-            printStatus(statusCodes, size)
-
-        stlist = line.split()
-        count += 1
-
-        try:
-            size += int(stlist[-1])
-        except Exception:
-            pass
-
-        try:
-            if stlist[-2] in statusCodes:
-                statusCodes[stlist[-2]] += 1
-        except Exception:
-            pass
-    printStatus(statusCodes, size)
+def status_printer(total_size, status):
+    """A method to print the status with the format given"""
+    print('File size: {}'.format(total_size))
+    for code, count in sorted(status.items()):
+        if count > 0:
+            print('{}: {}'.format(code, count))
 
 
-except KeyboardInterrupt:
-    printStatus(statusCodes, size)
-    raise
+def main():
+    """Main method"""
+    status_codes = {'200': 0, '301': 0, '400': 0,
+                    '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
+    total = 0
+    try:
+        count = 1
+        for line in stdin:
+            splited = line.split()
+            code = splited[-2]
+            if code in status_codes.keys():
+                status_codes[code] = status_codes[code] + 1
+            try:
+                total += int(splited[-1])
+            except Exception:
+                pass
+            if count % 10 == 0:
+                status_printer(total_size=total, status=status_codes)
+                for code in status_codes.keys():
+                    status_codes[code] = 0
+                total = 0
+            count += 1
+    except KeyboardInterrupt:
+        status_printer(total, status_codes)
+        raise
+
+
+if __name__ == '__main__':
+    main()
